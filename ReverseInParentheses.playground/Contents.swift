dbccdafb0521ad7088c65dd021b6b20c062935e4
Felipe Menezes <fmmobile@live.com>
2022-03-13 12:32:34 -0300

    ReverseInParentheses using stack solution

--- ReverseInParentheses.playground/Contents.swift
+++ ReverseInParentheses.playground/Contents.swift
@@ -2,11 +2,11 @@ import XCTest
 
 /*
  Write a function that reverses characters in (possibly nested) parentheses in the input string.
-
+ 
  Input strings will always be well-formed with matching ()s.
-
+ 
  Example
-
+ 
  For inputString = "(bar)", the output should be
  solution(inputString) = "rab";
  For inputString = "foo(bar)baz", the output should be
@@ -17,18 +17,18 @@ import XCTest
  solution(inputString) = "foobazrabblim".
  Because "foo(bar(baz))blim" becomes "foo(barzab)blim" and then "foobazrabblim".
  Input/Output
-
+ 
  [execution time limit] 6 seconds (swift)
-
+ 
  [input] string inputString
-
+ 
  A string consisting of lowercase English letters and the characters ( and ). It is guaranteed that all parentheses in inputString form a regular bracket sequence.
-
+ 
  Guaranteed constraints:
  0 ≤ inputString.length ≤ 50.
-
+ 
  [output] string
-
+ 
  Return inputString, with all the characters that were in parentheses reversed.
  
  */
@@ -59,11 +59,77 @@ func solution(inputString: String) -> String {
             return leftString + String(ritghString)
         }
         return String(input)
-   }
+    }
     return reverseBuffer(Array(inputString))
 }
 
-//XCTAssertEqual(solution(inputString: "(bar)"), "rab")
-XCTAssertEqual(solution(inputString: "foo(bar(baz))blim"), "foobazrabblim")
-//XCTAssertEqual(solution(inputString: "(abc)d(efg)"), "cbadgfe")
-//XCTAssertEqual(solution(inputString: "foo(bar)baz(blim)"), "foorabbazmilb")
+/*
+ Using Stack FIFO
+ Time Complexity O(n) | Space O(n)
+ */
+func solutionStack(inputString: String) -> String {
+    let open: Character = "("
+    let close: Character = ")"
+    guard inputString.count > 0 else { return "" }
+    var instructions: [(Int, Int)] = []
+    let chars = Array(inputString)
+    var resultString = chars
+    for index in 0...chars.count - 1 {
+        let char = chars[index]
+        if char == open {
+            instructions.append((index, 0))
+        }
+        
+        if char == close {
+            var position = instructions.removeLast()
+            position = (position.0, index)
+            // do the conversion
+            //print(position)
+            let lastPos = (position.1 - 1)  // positions without parenthesis
+            let firstPos = (position.0 + 1) // positions without parenthesis
+            var reversed: [Character] = []
+            for idx in 0...lastPos-firstPos {
+                reversed.append(resultString[lastPos - idx])
+            }
+            resultString.replaceSubrange(position.0 + 1...position.1 - 1, with: reversed)
+            //print(resultString)
+        }
+    }
+    let result = resultString.filter { char in
+        char != open && char != close
+    }
+    return String(result)
+}
+
+class TestCase: XCTestCase {
+    
+    func testSolution() {
+        XCTAssertEqual(solution(inputString: "(bar)"), "rab")
+        XCTAssertEqual(solution(inputString: "foo(bar(baz))blim"), "foobazrabblim")
+        XCTAssertEqual(solution(inputString: "(abc)d(efg)"), "cbadgfe")
+        XCTAssertEqual(solution(inputString: "foo(bar)baz(blim)"), "foorabbazmilb")
+    }
+    
+    func testSolutionStack() {
+        XCTAssertEqual(solutionStack(inputString: "(bar)"), "rab")
+        XCTAssertEqual(solutionStack(inputString: "foo(bar(baz))blim"), "foobazrabblim")
+        XCTAssertEqual(solutionStack(inputString: "(abc)d(efg)"), "cbadgfe")
+        XCTAssertEqual(solutionStack(inputString: "foo(bar)baz(blim)"), "foorabbazmilb")
+    }
+    
+    func testPerformanceSolution1() {
+        measure {
+            _ = solution(inputString: "foo(bar(baz))blim")
+        }
+    }
+    
+    func testPerformanceSolutionStack() {
+        measure {
+            _ = solutionStack(inputString: "foo(bar(baz))blim")
+        }
+    }
+}
+
+solutionStack(inputString: "(bar)")
+solutionStack(inputString: "foo(bar(baz))blim")
+TestCase.defaultTestSuite.run()
